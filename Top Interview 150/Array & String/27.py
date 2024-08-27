5ea218bbd27d15ece907aaa95c2147aea95e651c
Christian Monsalve <chrismon850@gmail.com>
2024-08-27 19:55:47 -0400

    problem 27

--- Top Interview 150/Array & String/27.py
+++ Top Interview 150/Array & String/27.py
@@ -1,17 +1,33 @@
-# Remove Element
+#27 - Remove Element
 
 class Solution(object):
-    # Time: Space: 
+    # Time: O(n) Space: O(1)
     def removeElement(self, nums, val):
         """
         :type nums: List[int]
         :type val: int
         :rtype: int
         """
-        
+        head = 0
+        tail = len(nums)-1
+        k = len(nums)
 
-nums = [3,2,2,3]
-val = 3
+        while head < tail:
+            if nums[tail] == val:
+                nums[tail] = None
+                tail -= 1
+                k -= 1
 
-solution = Solution()
-solution.removeElement()
+            if nums[head] == val:
+                nums[head] = nums[tail]
+                nums[tail] = None
+                tail -= 1
+                k -= 1
+            else:
+                head += 1
+
+        if len(nums) > 0 and nums[head] == val:
+            nums[head] == None
+            k -= 1
+            
+        return k
